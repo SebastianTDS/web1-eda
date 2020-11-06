031a3d1a9142f3b0b78ae599f57f74193b7031bb
Sebastian Da Silva <sebastianjds.99@gmail.com>
2020-11-06 08:18:01 -0300

    Codigo comentado

--- js/envioFormulario.js
+++ js/envioFormulario.js
@@ -1,14 +1,18 @@
+
+// Llama al evento DOMContentLoaded que se ejecuta cuando se termina 
+// de armar el arbol del DOM
 document.addEventListener("DOMContentLoaded", function (event) {
-    if (localStorage.getItem('mensaje') == 1) {
-        crearCasillaMensaje();
+    if (localStorage.getItem('mensaje')) {      //- Busca la variable mensaje en local
+        crearCasillaMensaje();                  //- Si la encuentra crea el pop-up de los sintomas
     }
-    localStorage.removeItem('mensaje');
+    localStorage.removeItem('mensaje');         //- Elimina mensaje del almacenamiento local
 
     var submit = document.getElementById("enviarForm");
 
-    submit.onsubmit = function () { registrarSintomas(); };
+    submit.onsubmit = function () { registrarSintomas(); };  // Llama un metodo al enviar el formulario
 });
 
+// Guarda las respuestas "Si" de los sintomas y una validación de envio para cuando se recargue la pagina 
 function registrarSintomas() {
     var datos = new Array();
     datos.push(document.getElementById('38?Y').checked)
@@ -17,10 +21,17 @@ function registrarSintomas() {
     datos.push(document.getElementById('dg?Y').checked)
     datos.push(document.getElementById('dr?Y').checked)
 
-    localStorage.setItem('mensaje', 1);
+    localStorage.setItem('mensaje', true);
     localStorage.setItem('datos', datos);
 }
 
+/*
+ Crea el pop-up para mostrar el resultado de los sintomas enviado el formulario:
+ - Crea un fondo que tapa todos los elementos debajo para evitar su interaccion.
+ - Impide el scroll en la pagina actual hasta no cerrar el pop-up o recargar.
+ - Muestra el recuadro del pop-up con el total de sintomas y cuales.
+ - Asigna un boton para cerrar el pop-up y seguir navegando en el sitio.
+*/
 function crearCasillaMensaje() {
     var mainContainer = document.createElement("div");
     var textContainer = document.createElement("div");
@@ -62,12 +73,14 @@ function crearCasillaMensaje() {
     localStorage.removeItem('datos');
 }
 
+// Cierra el pop-up.
 function cerrarVentana(a, b) {
     a.remove();
     b.remove();
     document.body.removeAttribute("style");
 }
 
+// Desplegable con el resultado de las preguntas de sintomas.
 function mostrar(a, b){
     if(a.innerHTML == "Mostrar más"){
         a.innerHTML = "Mostrar menos";
