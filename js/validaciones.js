b0e8c34a9ee13a42e816674073cc3f249126509e
Sebastian Da Silva <sebastianjds.99@gmail.com>
2020-11-06 12:35:28 -0300

    twicks 2

--- js/validaciones.js
+++ js/validaciones.js
@@ -16,43 +16,43 @@ const campos = {
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "user":
-			
-			if(expresiones.usuario.test(e.target.value)){
+
+			if (expresiones.usuario.test(e.target.value)) {
 				document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__usuario .formulario__input-error').classList.remove('formulario__input-error-activo');
-				campos['usuario']=true;
-			}else {
+				campos['usuario'] = true;
+			} else {
 				document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__usuario .formulario__input-error').classList.add('formulario__input-error-activo');
-				campos['usuario']=false;
+				campos['usuario'] = false;
 			}
-		break;
+			break;
 
 		case "email":
-			
-			if(expresiones.correo.test(e.target.value)){
+
+			if (expresiones.correo.test(e.target.value)) {
 				document.getElementById('grupo__correo').classList.remove('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__correo .formulario__input-error').classList.remove('formulario__input-error-activo');
-				campos['correo']=true;
-			}else {
+				campos['correo'] = true;
+			} else {
 				document.getElementById('grupo__correo').classList.add('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__correo .formulario__input-error').classList.add('formulario__input-error-activo');
-				campos['correo']=false;
+				campos['correo'] = false;
 			}
-		break;
+			break;
 
 		case "telefono":
-			
-			if(expresiones.telefono.test(e.target.value)){
+
+			if (expresiones.telefono.test(e.target.value)) {
 				document.getElementById('grupo__telefono').classList.remove('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__telefono .formulario__input-error').classList.remove('formulario__input-error-activo');
-				campos['telefono']=true;
-			}else {
+				campos['telefono'] = true;
+			} else {
 				document.getElementById('grupo__telefono').classList.add('formulario__grupo-incorrecto');
 				document.querySelector('#grupo__telefono .formulario__input-error').classList.add('formulario__input-error-activo');
-				campos['telefono']=false;
+				campos['telefono'] = false;
 			}
-		break;
+			break;
 	}
 }
 
@@ -63,42 +63,42 @@ inputs.forEach((input) => {
 });
 
 // Si el formulario esta validado se envia sino muestra un mensaje de error
-formulario.addEventListener('submit', function(e) {
-	e.preventDefault();
+formulario.onsubmit = function (e) {
 
-	if(campos.usuario && campos.correo && campos.telefono){
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-	}  else {
-		formulario.reset();
+	if(!campos.usuario || !campos.correo || !campos.telefono){
+		e.preventDefault();
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+
+		document.getElementById('grupo__usuario').classList.add('formulario__grupo-incorrecto');
+		document.querySelector('#grupo__usuario .formulario__input-error').classList.add('formulario__input-error-activo');
+		
+		document.getElementById('grupo__correo').classList.add('formulario__grupo-incorrecto');
+		document.querySelector('#grupo__correo .formulario__input-error').classList.add('formulario__input-error-activo');
+		
+		document.getElementById('grupo__telefono').classList.add('formulario__grupo-incorrecto');
+		document.querySelector('#grupo__telefono .formulario__input-error').classList.add('formulario__input-error-activo');
 
 		setTimeout(() => {
 			document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
 		}, 5000);
 	}
 
-});
+};
 
 //----contador de caracteres-----
-function getID(id){
+function getID(id) {
 	return document.getElementById(id).value;
 }
 
-function innerHTML(id,result){
-	return document.getElementById(id).innerHTML=result;
+function innerHTML(id, result) {
+	return document.getElementById(id).innerHTML = result;
 }
 
-function contadorCaracteres(){
-	setInterval(function(){
+function contadorCaracteres() {
+	setInterval(function () {
 		document.getElementById('coment').maxLength = 1000;
 		var c = getID("coment");
 		innerHTML("txtVista", (1000 - c.length) + " Caracteres restantes");
 		campos['comentario'] = true;
-		
-	},0000);
+	}, 0000);
 }
